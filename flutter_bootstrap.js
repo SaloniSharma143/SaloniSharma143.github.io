da26cc64de14f89a9e5e7d07a2159cdbb9cb5c25
anvesh <anveshmishra25@gmail.com>
2024-08-12 00:37:08 +0530

    created build from desktop scaling

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2447642937"
+    serviceWorkerVersion: "2213781254"
   }
 });
