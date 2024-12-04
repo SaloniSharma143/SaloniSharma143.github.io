5b58c00a38bf0a719bd899819e8ab4f5c8c8d8d9
anvesh <anveshmishra25@gmail.com>
2024-12-04 21:11:40 +0530

    fixed: grey screen issue

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2213781254"
+    serviceWorkerVersion: "3985364579"
   }
 });
